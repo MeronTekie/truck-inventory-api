45bd5b2ac03e28ba91cdebdd1cc6a87e28d36cbb
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-06-08 21:07:05 -0700

    Add a feature to select by query
    co-authored-by: Jake Edgar <jakeedgar1012@gmail.com>
    Co-authored-by:  Meron G.Tekie <mtekiesd210309@gmail.com>

--- TruckInventory/Controllers/InventoriesController.cs
+++ TruckInventory/Controllers/InventoriesController.cs
@@ -16,16 +16,35 @@ namespace TruckInventory.Controllers
       _db =db;
     }
     [HttpGet]
-    public async  Task<ActionResult<IEnumerable<Inventory>>> Get()
+    public async  Task<ActionResult<IEnumerable<Inventory>>> Get(string dispatchername,string driverid,string dispatcherid,string drivername)
+    // public async  Task<List<Inventory>> Get(string dispatchername,string driverid,string dispatcherid,string drivername)
     {
-      var inventory = await _db.Inventories.ToListAsync();
-      return inventory;
+    
+      var query =   _db.Inventories.AsQueryable();
+
+      if(driverid != null)
+      {
+        query = query.Where(e=>e.DriverId == driverid);
+      }
+      if(drivername != null)
+      {
+        query = query.Where(e=>e.DriverName == drivername);
+      }
+      if(dispatcherid != null)
+      {
+        query = query.Where(e=>e.DispatcherId == dispatcherid);
+      }
+      if(dispatchername != null)
+      {
+        query = query.Where(e=>e.DispatcherName == dispatchername);
+      }
+      return await query.ToListAsync();
     }
     [HttpPost]
     public async Task<ActionResult<Inventory>> Post(Inventory inventory)
     {
       _db.Inventories.Add(inventory);
-      _db.SaveChanges();
+      await _db.SaveChangesAsync();
       return CreatedAtAction("Post",new {id =inventory.InventoryId},inventory);
 
     }
@@ -41,7 +60,7 @@ namespace TruckInventory.Controllers
 
 
     }
-    [HttpPut]
+    [HttpPut("{id}")]
     public async Task<IActionResult> Put(int id, Inventory inventory)
     {
       if(id!= inventory.InventoryId)
@@ -71,6 +90,21 @@ namespace TruckInventory.Controllers
     private bool InventoryExists( int id)
     {
       return _db.Inventories.Any(e => e.InventoryId ==id);
+    }
+
+    [HttpDelete("{id}")]
+
+    public async Task<IActionResult> DeleteInventory(int id)
+    {
+      var inventory =  await _db.Inventories.FindAsync(id);
+      if(inventory==null)
+        {
+          return NotFound();
+        }
+      _db.Inventories.Remove(inventory);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
     }
   
 
